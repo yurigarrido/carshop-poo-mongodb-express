eaf71893f93d7d7851ae7066078b23ff8159df35
Yuri Garrido <y.garrido.yuri@gmail.com>
2022-09-15 22:49:55 -0300

    feat: add IVehicle interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const lensZodSchema = z.object({
+  model: z.string().min(3, { message: 'mui pequeno' }),
+  year: z.number().gte(1900, { message: 'mui novo' }).lte(2022, { message: 'futuro?' }),
+  color: z.string().min(3, { message: 'mui pequeno' }),
+  status: z.boolean(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof lensZodSchema>;
+
+export { lensZodSchema };
